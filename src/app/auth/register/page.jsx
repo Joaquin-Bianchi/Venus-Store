7c776f1410323b5a9655f822f991343c438b2b29
Joaquin <joaqobianchi13@gmail.com>
2024-03-11 18:55:27 -0300

    Add: Estructura inicial Pagina de registro

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <main className="flex h-screen m-auto border-t border-zinc-400">
+      <div className="w-5/12">
+        <img
+          className="object-cover object-top object-center w-full h-full"
+          src="/assets/images/login.png"
+          alt=""
+        />
+      </div>
+      <div className=" w-1/2 px-20 flex flex-col items-center">
+        <h1 className="text-4xl mt-10 ">Register</h1>
+        <form className="flex flex-col gap-1 w-full" action="">
+          <label htmlFor="">Name</label>
+          <input className="w-full bg-gray-100" type="email" required />
+
+          <label htmlFor="">Email</label>
+          <input className="w-full bg-gray-100" type="email" required />
+
+          <label htmlFor="">contraseña</label>
+          <input className="w-full bg-gray-100" type="password" required />
+
+          <button className="bg-violet-600 text-white">Enviar</button>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default page;
