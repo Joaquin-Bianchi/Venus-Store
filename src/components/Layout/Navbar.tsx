df3bb8c55b5ce25c8bc5613fe1bf337164fa45cc
Joaquin <joaqobianchi13@gmail.com>
2024-05-08 20:51:07 -0300

    add login and register with kinde

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { IoMdHeartEmpty } from "react-icons/io";
-import { RiUserLine } from "react-icons/ri";
 import { IoCartOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
+import { RiUserLine } from "react-icons/ri";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-function Navbar() {
-  const buttonNavbar =
-    "px-2 py-2 rounded-xl hover:bg-purple-600 hover:text-white ease-in-out duration-300 bg-gray-200";
-
+async function Navbar() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
   return (
     <header className="flex items-center w-full 2xl:px-36    text-black bg-slate-100 h-24 overflow-hidden">
       <nav className="flex items-center justify-between w-full">
@@ -25,24 +24,52 @@ function Navbar() {
           <Link href="">Combos</Link>
           <Link href="">Joggers</Link>
         </div>
-        <div className=" items-center gap-3 hidden lg:flex bg-gray-200 border border-gray-300 py-2 px-3 rounded-md">
-          <CiSearch className=" text-gray-700" />
+        <div className=" items-center text-gray-700 gap-3 hidden lg:flex bg-gray-200 border border-gray-300 py-2 px-3 rounded-md">
+          <CiSearch />
           <input
             className="bg-gray-200  outline-none font-medium "
             type="text"
             placeholder="Search"
           />
         </div>
-        <div className="hidden lg:flex  gap-3 jus items-center text-slate-600 text-2xl">
-          {/* agregar condicional si esta logueado o no */}
-
-          <Link href="" className={buttonNavbar}>
+        <div className="hidden lg:flex items-center text-slate-600 text-2xl">
+          {/* <Link href="" className={buttonNavbar}>
+          fav
             <IoMdHeartEmpty />
-          </Link>
-          <Link href="/auth/register" className={buttonNavbar}>
-            <RiUserLine />
-          </Link>
-          <Link href="" className={buttonNavbar}>
+          </Link> */}
+
+          <div className="flex items-center gap-3 ">
+            {user ? (
+              <>
+                <Link
+                  href=""
+                  className="bg-gray-200 ease-in-out text-3xl duration-300 text-black px-3 py-2  rounded-lg   gap-1 font-medium flex items-center"
+                >
+                  <RiUserLine />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/api/auth/register"
+                  className=" text-black  py-3  rounded-lg text-sm  font-medium flex items-center"
+                >
+                  Iniciar Sesion
+                </Link>
+                <Link
+                  href="/api/auth/login"
+                  className="bg-purple-600 text-white px-5 py-3  rounded-lg text-sm  font-medium flex items-center"
+                >
+                  Registrarse
+                </Link>
+              </>
+            )}
+          </div>
+          <div className="h-14 w-px bg-zinc-400 ml-3 mr-3"></div>
+          <Link
+            href=""
+            className="  bg-gray-200 ease-in-out text-3xl duration-300 text-black px-3 py-2  rounded-lg   gap-1 font-medium flex items-center"
+          >
             <IoCartOutline />
           </Link>
         </div>
