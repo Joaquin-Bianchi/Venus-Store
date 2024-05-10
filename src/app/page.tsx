c09f5865c22ebd749afa7660bb376513fd916ccd
Joaquin <joaqobianchi13@gmail.com>
2024-05-10 06:14:48 -0300

    feat: add newsletter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import CardHero from "../components/Cards/CardHero";
 import Footer from "../components/Sections/Footer";
 import CategoriesForMen from "../components/Sections/CategoriesForMen";
 import CategoriesForWomen from "../components/Sections/CategoriesForWomen";
-import Feedback from "../components/Sections/Feedback";
+import Feedback from "../components/Sections/Newsletter";
 import Ofertas from "../components/Sections/Oferts";
 import SliderMain from "../components/Sliders/SliderMain";
 import SliderNewArrival from "../components/Sliders/SliderNewArrival";
--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="px-4 divide-y dark:bg-black dark:text-gray-100 mt-48">
+    <footer className="px-4 divide-y dark:bg-zinc-950 dark:text-gray-100 ">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3 flex items-center">
           <a
