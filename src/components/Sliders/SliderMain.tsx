9f8013d9c0de1e64e2a7d5b459ab2198ef9a5655
Joaquin <joaqobianchi13@gmail.com>
2024-05-10 02:00:50 -0300

    Feat add Flowwbite lib

--- src/components/Sliders/SliderMain.tsx
+++ src/components/Sliders/SliderMain.tsx
@@ -1,6 +1,33 @@
+"use client";
+import { Carousel } from "flowbite-react";
 import React from "react";
 function SliderMain() {
-  return <div className="w-full bg-blue-600 h-[550px]">SliderMain</div>;
+  return (
+    <div className="h-56 sm:h-64 xl:h-[380px] 2xl:h-[596px]">
+      <Carousel slideInterval={2000}>
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+          alt="..."
+        />
+        <img
+          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+          alt="..."
+        />
+      </Carousel>
+    </div>
+  );
 }
 
 export default SliderMain;
