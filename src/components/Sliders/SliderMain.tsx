9192e2fb5fea7afc245a7bb035a2eda16cffb79d
Joaquin <joaqobianchi13@gmail.com>
2024-05-29 16:23:51 -0300

    Add Image to slider main

--- src/components/Sliders/SliderMain.tsx
+++ src/components/Sliders/SliderMain.tsx
@@ -6,7 +6,7 @@ function SliderMain() {
   return (
     <div className="h-[350px] sm:[500px] md:[540px]  xl:h-[680px] 2xl:h-[706px] m-auto">
       <Carousel className="w-full" slideInterval={600000000}>
-        <Link className="h-full" href="shop">
+        <Link className="h-full" href="/shop">
           <img
             className="w-full h-full object-left 2xl:object-top object-cover "
             src="/assets/images/slider-1.jpg"
